363228a93b2702e24a71bc6abfa8fe769d63664f
Anderson Campolina <andersoncampolina1@gmail.com>
2022-10-29 15:41:13 -0300

    Deploy automatizado concluido - Faz o deploy, minta para a carteira que fez o deploy e adiciona conquista automaticamente em uma funcao

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,20 +11,15 @@ async function main() {
   console.log("CarteirinhaNFT deployed to:", token.address);
   //console.log(token);
 
-  console.log(await token.name())
-  //const deployer_wallet = signer.SignerWithAddress.address;
-  //console.log(deployer_wallet);
+  owner_wallet = await token.owner();
+  console.log("Owner Wallet:", owner_wallet);
+  await token.safeMint(owner_wallet);
+  
+  const nomeConquista = "bola_automatica";
+  const dataConquista = 29102022;
+  const idConquista = 001;
 
-  //contractObject = await new ethers.Contract(deployedContract, abi, provider)
-
-  // copiando e colando bytecode da polygonscan (necessario apenas para fazer deploy) - o bytecode eh o codigo fonte do contrato transformado em linguagem de maquina, que eh a linguagem que o EVM executa
-  // const ContractByteCode = require("../../abiByteCode/byteCode.json")
-  // const factory = new ethers.ContractFactory(abi, ContractByteCode, signer) //para deploy, eh necessario a abi, o bytecode e a assinatura da metamask conectada
-  // const contrato = await factory.deploy(); //executa a funcao de deploy de fato
-  // const dados_deploy = await contrato.deployTransaction.wait();
-  // console.log(`Contrato deployado com sucesso da blockchain! Endereco do contrato: ${dados_deploy.contractAddress} e o txhash da transacao: ${dados_deploy.transactionash}`)
-  // deployedContract = await dados_deploy.contractAddress
-  // contractObject = await new ethers.Contract(deployedContract, abi, provider)
+  await token.adicionarConquistaHistorico(nomeConquista, dataConquista, idConquista);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
