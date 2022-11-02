2790832c7caaaf9d8c476f62e657f0095b349f94
Anderson Campolina <andersoncampolina1@gmail.com>
2022-11-02 13:50:12 -0300

    criei funcao que trata os dados das conquistas recebidas da blockchain

--- scripts/deploy_carteirinhaV2.js
+++ scripts/deploy_carteirinhaV2.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 const conquistas =[
   // {nomeConquista: "bola", idConquista: "001", dataCriadoBlockchain: "11101984"}
 
-  {nomeConquista: "[a, akjjd, ksajdakdjkajsd, askldjaskjdkajsdja, asldkaskldjlasdkja, askdaskdj, asdkjaskdjaskdjalskdjalksdj, aksjdjk, dsjklj], [asdhasdhjasd, asdjaskjasdk, jhkd, kjasdajksdkjasdkasdkj, kjsd]", idConquista: "[1, 2, 3, 4, 5, 6, 7, 8, 9], [10, 11, 12, 13, 14]", dataCriadoBlockchain: "[11101984, 11101984, 11101984, 11101984, 11101984, 11101984, 11101984, 11101984, 11101984], [11101984, 11101984, 11101984, 11101984, 11101984]"},
-  {nomeConquista: "[a, akjjd, ksajdakdjkajsd, askldjaskjdkajsdja, asldkaskldjlasdkja, askdaskdj, asdkjaskdjaskdjalskdjalksdj, aksjdjk, dsjklj], [asdhasdhjasd, asdjaskjasdk, jhkd, kjasdajksdkjasdkasdkj, kjsd]", idConquista: "[1, 2, 3, 4, 5, 6, 7, 8, 9], [10, 11, 12, 13, 14]", dataCriadoBlockchain: "[11101984, 11101984, 11101984, 11101984, 11101984, 11101984, 11101984, 11101984, 11101984], [11101984, 11101984, 11101984, 11101984, 11101984]"},
-  {nomeConquista: "[a, akjjd, ksajdakdjkajsd, askldjaskjdkajsdja, asldkaskldjlasdkja, askdaskdj, asdkjaskdjaskdjalskdjalksdj, aksjdjk, dsjklj], [asdhasdhjasd, asdjaskjasdk, jhkd, kjasdajksdkjasdkasdkj, kjsd]", idConquista: "[1, 2, 3, 4, 5, 6, 7, 8, 9], [10, 11, 12, 13, 14]", dataCriadoBlockchain: "[11101984, 11101984, 11101984, 11101984, 11101984, 11101984, 11101984, 11101984, 11101984], [11101984, 11101984, 11101984, 11101984, 11101984]"}
+  {nomeConquista: "[a,akjjd,ksajdakdjkajsd,askldjaskjdkajsdja,asldkaskldjlasdkja,askdaskdj,asdkjaskdjaskdjalskdjalksdj,aksjdjk,dsjklj]-[asdhasdhjasd,asdjaskjasdk,jhkd,kjasdajksdkjasdkasdkj,kjsd]", idConquista: "[1,2,3,4,5,6,7,8,9]-[10,11,12,13,14]", dataCriadoBlockchain: "[11101984,11101984,11101984,11101984,11101984,11101984,11101984,11101984,11101984]-[11101984,11101984,11101984,11101984,11101984]"},
+  {nomeConquista: "[a,akjjd,ksajdakdjkajsd,askldjaskjdkajsdja,asldkaskldjlasdkja,askdaskdj,asdkjaskdjaskdjalskdjalksdj,aksjdjk,dsjklj]-[asdhasdhjasd,asdjaskjasdk,jhkd,kjasdajksdkjasdkasdkj,kjsd]", idConquista: "[1,2,3,4,5,6,7,8,9]-[10,11,12,13,14]", dataCriadoBlockchain: "[11101984,11101984,11101984,11101984,11101984,11101984,11101984,11101984,11101984]-[11101984,11101984,11101984,11101984,11101984]"},
+  {nomeConquista: "[a,akjjd,ksajdakdjkajsd,askldjaskjdkajsdja,asldkaskldjlasdkja,askdaskdj,asdkjaskdjaskdjalskdjalksdj,aksjdjk,dsjklj]-[asdhasdhjasd,asdjaskjasdk,jhkd,kjasdajksdkjasdkasdkj,kjsd]", idConquista: "[1,2,3,4,5,6,7,8,9]-[10,11,12,13,14]", dataCriadoBlockchain: "[11101984,11101984,11101984,11101984,11101984,11101984,11101984,11101984,11101984]-[11101984,11101984,11101984,11101984,11101984]"}
 
 ]
 
@@ -27,7 +27,7 @@ async function main() {
   await token.deployed();
   //console.log(token);
 
-  const tokenAddress = token.address + ", ";
+  const tokenAddress = "," + token.address;
   console.log("CarteirinhaV2 deployed to:", tokenAddress);
 
 
--- scripts/interagir_carteirinhaV2.js
+++ scripts/interagir_carteirinhaV2.js
@@ -2,7 +2,8 @@ require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 var fs = require('fs');
 //const util = require('util');
-var ethers = require('ethers')
+var ethers = require('ethers');
+const { id } = require("ethers/lib/utils");
 const fsPromises = fs.promises;
 
 // The path to the contract ABI
@@ -14,17 +15,16 @@ const DEPLOYED_CONTRACTS_ADDRESS_FILE_PATH = './deployed_contracts_address.txt';
 async function getAbi(){ 
   const data = await fsPromises.readFile(ABI_FILE_PATH, 'utf8');
   const abi = JSON.parse(data)['abi'];
-  console.log(abi);
+  //console.log(abi);
   return abi;
 }
 // const data = fs.readFileSync('./input.txt',
 //             {encoding:'utf8', flag:'r'});
 
 async function getLastContractDeployedAddress(){ 
-    const data = await fsPromises.readFileSync(DEPLOYED_CONTRACTS_ADDRESS_FILE_PATH, 'utf8');
-    const deployed_contracts_address_array = data.split(", ");
-    console.log(deployed_contracts_address_array[0])
-    return deployed_contracts_address_array[0];
+    const data = await fsPromises.readFile(DEPLOYED_CONTRACTS_ADDRESS_FILE_PATH, 'utf8');
+    const deployed_contracts_address_array = data.split(",");
+    return deployed_contracts_address_array[deployed_contracts_address_array.length - 1];
   }
 
 async function main() {
@@ -44,7 +44,7 @@ async function main() {
     const { PRIVATE_KEY } = process.env;
     let signer = new ethers.Wallet(PRIVATE_KEY, provider);
     const last_contract_deployed_address = await getLastContractDeployedAddress();
-    last_contract_deployed_address = "0xA8C1Ea1fa3E782a5063052238563c631d1b18A32"
+    //const last_contract_deployed_address = "0xC202EeEB5d3f12aA9D8e5C35ed1115564Fe538A7"
     const token = new ethers.Contract(last_contract_deployed_address, abi, signer);
 
     // let transacao = await token.adicionarConquistaHistorico("consegui", "23081984", "3");
@@ -55,7 +55,25 @@ async function main() {
     console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 
     const response = await token.consultarHistorico();
-    console.log(await response);
+    //console.log(await response[0].nomeConquista, response[0].dataConquista, response[0].idConquista);
+ 
+    async function trataConquista(tipoConquista) {
+      const ConquistaArrayStringSplit = await (response[0][tipoConquista]).split("-");
+      const ConquistaArr = [];
+
+      for(i=0; i<ConquistaArrayStringSplit.length; i++) {
+        ConquistaArr.push((ConquistaArrayStringSplit[i].replace("[","").replace("]","")).split(","));
+      }
+
+      for(i=0; i<ConquistaArr.length-1; i++) {
+        ConquistaArr2 = ConquistaArr[i].concat(ConquistaArr[i+1])
+      }
+      return ConquistaArr2;
+    }
+    const nomeConquistas = await trataConquista("nomeConquista");
+    const dataConquistas = await trataConquista("dataConquista");
+    const idConquistas = await trataConquista("idConquista");
+    console.log(nomeConquistas, dataConquistas, idConquistas)
 }
 
 main()
