e54734c527dbaa3001c8f7123eaa5fb16762fea4
Anderson Campolina <andersoncampolina1@gmail.com>
2022-11-07 17:27:53 -0300

    funcionalidade de cadastro na blockchain atualizado e funcionando

--- scripts/deploy_carteirinhaV2.js
+++ scripts/deploy_carteirinhaV2.js
@@ -2,25 +2,15 @@ const hre = require("hardhat");
 const fs = require('fs');
 const axios = require("axios");
 
-// const conquistas =[
-//   // {nomeConquista: "bola", idConquista: "001", dataCriadoBlockchain: "11101984"}
-
-//   {nomeConquista: "[a,akjjd,ksajdakdjkajsd,askldjaskjdkajsdja,asldkaskldjlasdkja,askdaskdj,asdkjaskdjaskdjalskdjalksdj,aksjdjk,dsjklj]-[asdhasdhjasd,asdjaskjasdk,jhkd,kjasdajksdkjasdkasdkj,kjsd]", idConquista: "[1,2,3,4,5,6,7,8,9]-[10,11,12,13,14]", dataCriadoBlockchain: "[11101984,11101984,11101984,11101984,11101984,11101984,11101984,11101984,11101984]-[11101984,11101984,11101984,11101984,11101984]"},
-//   {nomeConquista: "[a,akjjd,ksajdakdjkajsd,askldjaskjdkajsdja,asldkaskldjlasdkja,askdaskdj,asdkjaskdjaskdjalskdjalksdj,aksjdjk,dsjklj]-[asdhasdhjasd,asdjaskjasdk,jhkd,kjasdajksdkjasdkasdkj,kjsd]", idConquista: "[1,2,3,4,5,6,7,8,9]-[10,11,12,13,14]", dataCriadoBlockchain: "[11101984,11101984,11101984,11101984,11101984,11101984,11101984,11101984,11101984]-[11101984,11101984,11101984,11101984,11101984]"},
-//   {nomeConquista: "[a,akjjd,ksajdakdjkajsd,askldjaskjdkajsdja,asldkaskldjlasdkja,askdaskdj,asdkjaskdjaskdjalskdjalksdj,aksjdjk,dsjklj]-[asdhasdhjasd,asdjaskjasdk,jhkd,kjasdajksdkjasdkasdkj,kjsd]", idConquista: "[1,2,3,4,5,6,7,8,9]-[10,11,12,13,14]", dataCriadoBlockchain: "[11101984,11101984,11101984,11101984,11101984,11101984,11101984,11101984,11101984]-[11101984,11101984,11101984,11101984,11101984]"}
-
-// ]
-
-// const getAchievements = async () => {
-//   const response = await axios.get("http://localhost:3000/Conquistas")
-//   const conquistas = await response.data
-//   console.log(response.status, "Quantidade de conquistas "+ conquistas.length)
-//   return conquistas
-// };
+const getAchievements = async () => {
+  const response = await axios.get("http://localhost:3000/Conquistas")
+  const conquistasDb = await response.data
+  console.log(response.status, "Quantidade de conquistas " + conquistasDb.length)
+  return conquistasDb
+};
 
 async function main() {
 
-  const conquistas = await getAchievements();
   const TOKEN = await hre.ethers.getContractFactory("CarteirinhaV2");
   const token = await TOKEN.deploy();
 
@@ -38,30 +28,52 @@ async function main() {
   });
 
   owner_wallet = await token.owner();
-  console.log("Owner Wallet:", owner_wallet);
+  console.log("Owner Wallet:" + owner_wallet);
 
   await token.safeMint(owner_wallet);
 
   let maxTry = 3
   let multiply = 1;
 
-  for(let i = 0; i < conquistas.length && maxTry; i++) {
-    const conquistaAtual = conquistas[i]
-    const {nomeConquista, dataCriadoBlockchain, idConquista} = conquistaAtual
+  const txHashConquista = []
+  const conquistas = await getAchievements();
+
+  const nomeConquistaArr = [];
+  const dataConquistaArr = [];
+  const idConquistaArr = [];
+  conquistas.map((element) => {
+    nomeConquistaArr.push(element.nomeConquista);
+    dataConquistaArr.push(element.createdAt);
+    idConquistaArr.push(element.id.toString());
+  });
+
+  const nomeConquistaArray = []
+  const dataConquistaArray = []
+  const idConquistaArray = []
+
+  nomeConquistaArray.push(JSON.stringify(nomeConquistaArr))
+  dataConquistaArray.push(JSON.stringify(dataConquistaArr))
+  idConquistaArray.push(JSON.stringify(idConquistaArr))
+
+  for(let i = 0; i < nomeConquistaArray.length && maxTry; i++) {
+
+    const nomeConquista = nomeConquistaArray[i];
+    const dataConquista = dataConquistaArray[i];
+    const idConquista = idConquistaArray[i];
     setTimeout( async () => {
       if (maxTry > 0) {
         try {
-          const gravarConquista = await token.adicionarConquistaHistorico(nomeConquista, dataCriadoBlockchain, idConquista);
+          const gravarConquista = await token.adicionarConquistaHistorico(nomeConquista, dataConquista, idConquista);
           const conquistaDeployada = await gravarConquista.wait()
-          console.log(conquistaAtual.nomeConquista)
-          console.log(conquistaDeployada.transactionHash)
+          txHashConquista.push(conquistaDeployada.transactionHash)
+          console.log(txHashConquista[i])
         } catch (err) {
           console.log(err)
           multiply = multiply * 1.50;
           maxTry--;
         }
       } else {
-        i = conquistas.length + 1 
+        i = nomeConquistaArr.length + 1 
       }
     }, 1000 * i * multiply)
   }
---
+++ test/teste.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+
+const main = async () => {
+  const getAchievements = async () => {
+    const response = await axios.get("http://localhost:3000/Conquistas")
+    const conquistas = await response.data
+    console.log(response.status, "Quantidade de conquistas " + conquistas.length)
+    return conquistas
+  };
+
+  const conq = await getAchievements();
+
+  const nomeConquistaArr = [];
+  const dataConquistaArr = [];
+  const idConquistaArr = [];
+  conq.map((element) => {
+    nomeConquistaArr.push(element.nomeConquista);
+    dataConquistaArr.push(element.createdAt);
+    idConquistaArr.push(element.id.toString());
+  });
+
+  const nomeConquistaArray = []
+  const dataConquistaArray = []
+  const idConquistaArray = []
+
+  nomeConquistaArray.push(JSON.stringify(nomeConquistaArr))
+  dataConquistaArray.push(JSON.stringify(dataConquistaArr))
+  idConquistaArray.push(JSON.stringify(idConquistaArr))
+
+  console.log(nomeConquistaArray)
+  console.log(dataConquistaArray)
+  console.log(idConquistaArray)
+  console.log(nomeConquistaArray.length)
+
+}
+main()
