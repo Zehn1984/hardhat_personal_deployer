0b8b00be8664bf3f93569c8a45f07d7e99dc14a4
Anderson Carlos Campolina <zehn1984@Andersons-MacBook-Air.local>
2022-11-25 12:08:50 -0300

    mudando nome da variavel token para contract para facilitar o entendimento

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 
 async function main() {
     // faz o deploy do contrato que esta na pasta contracts
-    const TOKEN = await hre.ethers.getContractFactory("CarteirinhaNFT"); // colocar nome correto do contrato que esta na pasta contracts
-    const token = await TOKEN.deploy();
-    await token.deployed();
-    console.log("Deployed contract address:", token.address);
+    const CONTRACT = await hre.ethers.getContractFactory("CarteirinhaNFT"); // colocar nome correto do contrato que esta na pasta contracts
+    const contract = await CONTRACT.deploy();
+    await contract.deployed();
+    console.log("Deployed contract address:", contract.address);
 
     // armazena endereco do contrato criado no arquivo deployed_contracts_address
-    const tokenAddress = "," + token.address;
-    fs.appendFile('./deployed_contracts_address.txt', tokenAddress, err => {
+    const contractAddress = "," + contract.address;
+    fs.appendFile('./deployed_contracts_address.txt', contractAddress, err => {
         if (err) {
             console.error(err);
         }
