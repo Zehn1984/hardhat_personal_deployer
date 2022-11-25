5a5564958028a862a963c347891dd22c5cf1112a
Anderson Carlos Campolina <zehn1984@Andersons-MacBook-Air.local>
2022-11-25 12:06:48 -0300

    adicionando comentarios uteis

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,14 +2,14 @@ const hre = require("hardhat");
 const fs = require('fs');
 
 async function main() {
-    // colocar nome correto do contrato que esta na pasta contracts
-    const TOKEN = await hre.ethers.getContractFactory("CarteirinhaNFT");
+    // faz o deploy do contrato que esta na pasta contracts
+    const TOKEN = await hre.ethers.getContractFactory("CarteirinhaNFT"); // colocar nome correto do contrato que esta na pasta contracts
     const token = await TOKEN.deploy();
     await token.deployed();
     console.log("Deployed contract address:", token.address);
 
+    // armazena endereco do contrato criado no arquivo deployed_contracts_address
     const tokenAddress = "," + token.address;
-
     fs.appendFile('./deployed_contracts_address.txt', tokenAddress, err => {
         if (err) {
             console.error(err);
