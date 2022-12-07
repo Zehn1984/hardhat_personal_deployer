c91395067c405d808a738b9cf3673907136ffdf2
Anderson Carlos Campolina <zehn1984@Andersons-MacBook-Air.local>
2022-12-07 15:59:12 -0300

    Ultimas modificacoes. Interact generico com ABI automatica funcionando com sucesso

--- scripts/services/interact/interact.js
+++ scripts/services/interact/interact.js
@@ -5,24 +5,30 @@ var ethers = require('ethers')
 
 async function main() {
 
-    const contractAddress = "0xDceAA2FD8332Ed4eC3131CF26aff2687d385FdC0";
-    const chainId = 80001;
-    
+    // Passe o numero do contrato a ser interagido, e em qual chainId ele esta deployado
+    const contractAddress = "0xDceAA2FD8332Ed4eC3131CF26aff2687d385FdC0"; // <<-------
+    const chainId = 80001; // <<-------
+
     const data = await getAbi(contractAddress, chainId);
-    //console.log(data)
 
     const provider = ethers.getDefaultProvider(data.rpcUrl);
     const { PRIVATE_KEY } = process.env;
     const signer = new ethers.Wallet(PRIVATE_KEY, provider);
     const contract = new ethers.Contract(contractAddress, data.abi, signer);
-    console.log(contract);
-    const transaction = await contract.name();
+    // PASSE A FUNCAO DO CONTRATO QUE DESEJA EXECUTAR AQUI, NO LUGAR DO NAME()
+    const transaction = await contract.name(); // <<----------------------
 
-    console.log(transaction);
-    const txObj = await transaction.wait();
-    // const txHash = transaction.transactionHash;
-    // console.log("Comprovante da transacao: " + txHash);
-    console.log(txObj);
+    // Caso escreva (pague taxa) na blockchain, eh gerado um objeto de transacao
+    try {
+        const txObj = await transaction.wait();
+        const txHash = await txObj.transactionHash;
+        // console.log(txObj);
+        // console.log(transaction);
+        console.log("Comprovante da transacao: " + txHash);        
+    } catch (error) {
+        console.log(transaction);
+    }
+
 }
 
 main()
